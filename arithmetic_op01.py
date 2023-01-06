56156e2d748657b9843f8ce6e241e85e7ea3f0a6
norpulatovogabek <norpulatovogabek42@gmail.com>
2023-01-06 14:16:51 +0500

    arithmetic_op01.py

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -3,6 +3,6 @@ x = 10
 #Create a variable 'y' and assign it the value of 5.
 y = 5
 #Create a variable 'result' and assign it the value of x + y.
-result = x + y
+result = y + x
 #Print the value of the 'result'.
 print(result)
