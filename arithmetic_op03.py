99388fab06f36eabea35a766752fb9eda1054336
norpulatovogabek <norpulatovogabek42@gmail.com>
2023-01-06 14:29:19 +0500

    all

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,7 +1,7 @@
 #Create a variable called 'number' and assign it the two-digit number
 
 #Find the reverse of the number and assign it to a variable called 'answer'.
-#g
+#gk
 #Print the value of the 'answer'
 
 
